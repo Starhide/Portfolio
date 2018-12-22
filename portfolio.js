d669845bf163cb9af6dbd2adc090a570e8df32c0
Blake Bauer <blakebauer1997@gmail.com>
2018-12-21 23:15:15 -0800

    Main page reorganizing

--- portfolio.js
+++ portfolio.js
@@ -19,63 +19,58 @@ function textProgressBar(e, speed, length, i, max, prev) {
         $(e)[0].innerHTML = prev + "<pre>[" + "=".repeat(Math.floor((i / max) * length)) + " ".repeat(length - Math.floor((i / max) * length)) + "] " + i + "/" + max + "</pre>";
         i++;
         setTimeout(textProgressBar, speed, e, speed, length, i, max, prev);
-    }else {
+    } else {
         $(e)[0].innerHTML += " > ";
     }
 }
 
 
 function displayProjects() {
-    printSlowText("#Projects .console-inside", 25, "> print(links.Projects.description)", 0, function (e) {
-        $(e)[0].innerHTML += "<br />A collection of everything I've worked on. School projects, experiments, applications, etc. Some are finished, many are work-in-progress, and a few are abandoned.<br />";
-        setTimeout(printSlowText, 500, "#Projects .console-inside", 25, "> displayProjects();", 0, function (e) {
-            textProgressBar(e, projectLoadSpeed / projects.length, 20, 0, projects.length, $(e)[0].innerHTML);
-            var i = 0;
-            for (const project of projects) {
-                var element =
-                    `<div class=\"col-md-4\"><div class=\"project-tile\"><img src=\"${project.image}\" alt=\"\" class=\"project-image\">
+    var i = 0;
+    for (const project of projects) {
+        var element =
+            `<div class=\"col-md-4\"><div class=\"project-tile\"><img src=\"${project.image}\" alt=\"\" class=\"project-image\">
                      <div class=\"tile-text\">
                      <h2><a href="${project.link}">${project.title}</a></h2>
                      <p>${project.description}</p>
                      Tags: ${project.tags}</div></div></div>`;
-                $("#TileBoard > .row").append(element);
-                $($("#TileBoard .project-tile")[i]).delay(projectLoadSpeed / projects.length * (i+1)).fadeIn(1000);
-                i = i + 1;
-            }
-        });
-    });
+        $("#TileBoard > .row").append(element);
+        $($("#TileBoard .project-tile")[i]).delay(projectLoadSpeed / projects.length * (i + 1)).fadeIn(1000);
+        i = i + 1;
+    }
 }
 
-function showProjects(){
+function showProjects() {
     $("#Projects .console-inside")[0].innerHTML = "";
     $("#Projects").delay(1000).slideDown();
     setTimeout(displayProjects, 1000);
 }
 
-function hideProjects(){
+function hideProjects() {
     $("#TileBoard .project-tile").fadeOut();
-    
-    setTimeout(function(){
+
+    setTimeout(function () {
         $("#TileBoard > .row").empty();
     }, 1000);
 
     $("#Projects").delay(700).slideUp();
 }
 
-function showAbout(){
+function showAbout() {
     $("#About").delay(1400).slideDown();
 }
 
-function hideAbout(){
+function hideAbout() {
     $("#About").slideUp();
 }
 
-$(document).ready(function(){
-    if(window.location.hash) {
+$(document).ready(function () {
+    displayProjects()
+    if (window.location.hash) {
         var hash = window.location.hash.substring(1);
-        if(hash.toLowerCase() == "about"){
+        if (hash.toLowerCase() == "about") {
             $("#About").show();
-        } else if(hash.toLowerCase() == "projects") {
+        } else if (hash.toLowerCase() == "projects") {
             $("#Projects").show();
             displayProjects();
         }
